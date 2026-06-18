6c266e4ce123d944a5c46f491c7b618b56a5c2b4
YEO BIN ZHENG <2402076J@student.tp.edu.sg>
2026-06-18 22:06:29 +0800

    Create popup.js

---
+++ extension/popup.js
@@ -0,0 +1,33 @@
+document.getElementById('reportBtn').addEventListener('click', async () => {
+
+  // 1. Get the URL of the current active browser tab
+
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  let currentUrl = tab.url;
+
+
+
+  // 2. Send this URL to your backend (Replace with your actual backend URL later)
+
+  alert("Reporting: " + currentUrl); 
+
+  
+
+  /* Eventually, you will replace the alert above with code like this:
+
+  
+
+  fetch('http://localhost:5000/api/report', {
+
+    method: 'POST',
+
+    headers: { 'Content-Type': 'application/json' },
+
+    body: JSON.stringify({ url: currentUrl })
+
+  });
+
+  */
+
+});
